65df1044fac53ee4527307e09435e46ca5e975d8
Nishizaki <steelpipe75@gmail.com>
2024-02-25 23:12:10 +0900

    htmlソースをパースして改行を追加

--- darumaotoshi.py
+++ darumaotoshi.py
@@ -3,24 +3,32 @@ from html.parser import HTMLParser
 # HTMLをパースするためのクラスを定義
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
-        print("Start tag:", tag)
+        # print("Start tag:", tag)
+        outputfile.write('<' + tag)
         for attr in attrs:
             print("  Attribute:", attr)
+            outputfile.write(' ' + attr[0] + "='" + attr[1] + "'")
+        outputfile.write('>\n')
 
     def handle_endtag(self, tag):
-        print("End tag  :", tag)
+        # print("End tag  :", tag)
+        outputfile.write('</' + tag + '>\n')
 
     def handle_data(self, data):
-        print("Data     :", data)
+        # print("Data     :", data)
+        outputfile.write(data + '\n')
 
 # HTMLを取得
 html = ""
-with open(r'./tests/bowling_game_cli/index.html', 'r', encoding='utf-8') as file:
-    html = file.read()
-    print(html)
+with open(r'./output.html', 'w', encoding='utf-8') as outputfile:
+    with open(r'./tests/bowling_game_cli/index.html', 'r', encoding='utf-8') as inputfile:
+        html = inputfile.read()
+        # print(html)
 
-# HTMLをパース
-parser = MyHTMLParser()
-parser.feed(html)
-parser.close()
+    outputfile.write('<!doctype html>\n')
+
+    # HTMLをパース
+    parser = MyHTMLParser()
+    parser.feed(html)
+    parser.close()
 
