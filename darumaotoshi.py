e9e8a47b5930593b93ae831581569eb35b6913c1
Nishizaki <steelpipe75@gmail.com>
2024-02-26 19:48:54 +0900

    出力フォルダ作成、cssのコピーを対応

--- darumaotoshi.py
+++ darumaotoshi.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 from html.parser import HTMLParser
 
 # HTMLをパースするためのクラスを定義
@@ -6,9 +8,12 @@ class MyHTMLParser(HTMLParser):
         # print("Start tag:", tag)
         outputfile.write('<' + tag)
         for attr in attrs:
-            print("  Attribute:", attr)
+            # print("  Attribute:", attr)
             outputfile.write(' ' + attr[0] + "='" + attr[1] + "'")
-        outputfile.write('>\n')
+        if tag == 'a':
+            outputfile.write('>')
+        else:
+            outputfile.write('>\n')
 
     def handle_endtag(self, tag):
         # print("End tag  :", tag)
@@ -16,12 +21,28 @@ class MyHTMLParser(HTMLParser):
 
     def handle_data(self, data):
         # print("Data     :", data)
-        outputfile.write(data + '\n')
+        outputfile.write(data)
+
+def make_dir_helper(target_path):
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
 
 # HTMLを取得
-html = ""
-with open(r'./output.html', 'w', encoding='utf-8') as outputfile:
-    with open(r'./tests/bowling_game_cli/index.html', 'r', encoding='utf-8') as inputfile:
+html = ''
+input_index_html = './tests/bowling_game_cli/index.html'
+input_dir = os.path.dirname(input_index_html)
+# print(input_dir)
+
+output_dir = './output'
+make_dir_helper(output_dir)
+ret = shutil.copy(os.path.join(input_dir, 'style.css'), os.path.join(output_dir, 'style.css'))
+
+output_style_css = os.path.join(output_dir, 'style.css')
+# print(output_style_css)
+output_index_html = os.path.join(output_dir, 'index.html')
+# print(output_index_html)
+with open(output_index_html, 'w', encoding='utf-8') as outputfile:
+    with open(input_index_html, 'r', encoding='utf-8') as inputfile:
         html = inputfile.read()
         # print(html)
 
