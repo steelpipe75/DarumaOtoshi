d0410f457ac261414452f2023229c1bc528c0f27
Nishizaki <steelpipe75@gmail.com>
2024-02-29 21:22:39 +0900

    コマンドライン引数で入出力を指定

--- darumaotoshi.py
+++ darumaotoshi.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import re
+import argparse
 import html
 from html.parser import HTMLParser
 from io import TextIOWrapper
@@ -143,9 +144,25 @@ def darmaotoshi(input_index_html: str, output_dir: str):
         copy_coverage_html(src_path, dst_path, output_style_css_path)
 
 
+def arg_parse():
+    parser = argparse.ArgumentParser()
+
+    # 入力HTMLファイルを指定するオプションを追加
+    parser.add_argument('-i', '--input', dest = 'input_file', default = 'tests/bowling_game_cli/index.html',
+                        help='input HTML file')
+
+    # 出力ディレクトリを指定するオプションを追加
+    parser.add_argument('-o', '--output', dest = 'output_dir', default = 'output',
+                        help='output directory')
+
+    # コマンドライン引数のパース
+    args = parser.parse_args()
+
+    # パースされた引数を使って処理を行う
+    return args.input_file, args.output_dir
+
 
 if __name__ == '__main__':
-    input_index_html = './tests/bowling_game_cli/index.html'
-    output_dir = './output'
+    input_index_html, output_dir = arg_parse()
     darmaotoshi(input_index_html, output_dir)
 
