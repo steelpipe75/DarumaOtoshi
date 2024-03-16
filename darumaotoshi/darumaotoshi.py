e6199dd58e4ba418b8501486a258e2c4dd072e02
Yutaka Nishizaki <steelpipe75@gmail.com>
2024-03-16 00:44:20 +0000

    クラス変数のスコープ見直し

--- darumaotoshi/darumaotoshi.py
+++ darumaotoshi/darumaotoshi.py
@@ -10,88 +10,95 @@ from io import TextIOWrapper
 # index.htmlをパースするためのクラスを定義
 class indexHTMLParser(HTMLParser):
     def __init__(
-        self, out: TextIOWrapper,
-        *, convert_charrefs: bool = True
+        self, *, convert_charrefs: bool = True
     ) -> None:
         super().__init__(convert_charrefs=convert_charrefs)
-        self.files = []
-        self.file_info = {"href": "", "data": ""}
-        self.append_required = False
-        self.out = out
+        self.__files = []
+        self.__file_info = {"href": "", "data": ""}
+        self.__append_required = False
+        self.__outputstr = ""
 
     def handle_starttag(self, tag: str, attrs) -> None:
         # print("Start tag:", tag)
-        self.out.write("<" + tag)
+        self.__outputstr += ("<" + tag)
         if tag == "a":
             link_target = attrs[0][1]
             pattern = r"^coverage/"
             if re.search(pattern, link_target):
-                self.file_info["href"] = link_target
-                self.append_required = True
+                self.__file_info["href"] = link_target
+                self.__append_required = True
             else:
                 for attr in attrs:
                     # print("  Attribute:", attr)
-                    self.out.write(" " + attr[0] + "='" + attr[1] + "'")
-                self.out.write(">")
+                    self.__outputstr += (" " + attr[0] + "='" + attr[1] + "'")
+                self.__outputstr += (">")
         else:
             for attr in attrs:
                 # print("  Attribute:", attr)
-                self.out.write(" " + attr[0] + "='" + attr[1] + "'")
-            self.out.write(">")
+                self.__outputstr += (" " + attr[0] + "='" + attr[1] + "'")
+            self.__outputstr += (">")
 
     def handle_endtag(self, tag: str) -> None:
         # print("End tag  :", tag)
-        self.out.write("</" + tag + ">")
+        self.__outputstr += ("</" + tag + ">")
 
     def handle_data(self, data: str) -> None:
         # print("Data     :", data)
-        if self.append_required:
-            self.file_info["data"] = data
-            self.files.append(self.file_info)
+        if self.__append_required:
+            self.__file_info["data"] = data
+            self.__files.append(self.__file_info)
             cov_html_path = os.path.normpath(
                 os.path.join("coverage", data + ".html")
             ).replace("\\", "/")
             print(cov_html_path)
-            self.out.write(" href" + "='" + cov_html_path + "'>")
-        self.append_required = False
-        self.file_info = {"href": "", "data": ""}
-        self.out.write(html.escape(data))
+            self.__outputstr += (" href" + "='" + cov_html_path + "'>")
+        self.__append_required = False
+        self.__file_info = {"href": "", "data": ""}
+        self.__outputstr += (html.escape(data))
+
+    def get_files(self) -> str:
+        return self.__files
+
+    def get_outputstr(self) -> str:
+        return self.__outputstr
 
 
 # 各ソースのカバレッジデータhtmlをパースするためのクラスを定義
 class coverageHTMLParser(HTMLParser):
     def __init__(
-        self, css_path: str, out: TextIOWrapper,
-        *, convert_charrefs: bool = True
+        self, css_path: str, *, convert_charrefs: bool = True
     ) -> None:
         super().__init__(convert_charrefs=convert_charrefs)
-        self.out = out
-        self.css_path = css_path
+        self.__outputstr = ""
+        self.__css_path = css_path
 
     def handle_starttag(self, tag: str, attrs) -> None:
         # print("Start tag:", tag)
         if tag == "link":
-            self.out.write("<" + tag)
+            self.__outputstr += ("<" + tag)
             for attr in attrs:
                 # print("  Attribute:", attr)
                 if attr[0] == "href":
-                    self.out.write(" " + "href" + "='" + self.css_path + "'")
+                    self.__outputstr += (" " + "href" + "='" + self.__css_path + "'")
                 else:
-                    self.out.write(" " + attr[0] + "='" + attr[1] + "'")
-            self.out.write(">")
+                    self.__outputstr += (" " + attr[0] + "='" + attr[1] + "'")
+            self.__outputstr += (">")
         else:
-            self.out.write("<" + tag)
+            self.__outputstr += ("<" + tag)
             for attr in attrs:
                 # print("  Attribute:", attr)
-                self.out.write(" " + attr[0] + "='" + attr[1] + "'")
-            self.out.write(">")
+                self.__outputstr += (" " + attr[0] + "='" + attr[1] + "'")
+            self.__outputstr += (">")
 
     def handle_endtag(self, tag: str) -> None:
         # print("End tag  :", tag)
-        self.out.write("</" + tag + ">")
+        self.__outputstr += ("</" + tag + ">")
 
     def handle_data(self, data: str) -> None:
-        self.out.write(html.escape(data))
+        self.__outputstr += (html.escape(data))
+
+    def get_outputstr(self) -> str:
+        return self.__outputstr
 
 
 def copy_coverage_html(
@@ -117,8 +124,10 @@ def copy_coverage_html(
             dst_file.write("<!doctype html>")
 
             # HTMLをパース
-            cov_parser = coverageHTMLParser(css_path, dst_file)
+            cov_parser = coverageHTMLParser(css_path)
             cov_parser.feed(src_html)
+            outputstr = cov_parser.get_outputstr()
+            dst_file.write(outputstr)
             cov_parser.close()
 
 
@@ -136,6 +145,8 @@ def darumaotoshi(input_index_html: str, output_dir: str) -> None:
         (os.path.join(output_dir, "style.css")).replace("\\", "/")
     )
     print("*** " + src_path + " -> " + dst_path)
+    print("src_path = " + src_path)
+    print("dst_path = " + dst_path)
     shutil.copy(src_path, dst_path)
 
     output_style_css_path = os.path.normpath(
@@ -157,13 +168,15 @@ def darumaotoshi(input_index_html: str, output_dir: str) -> None:
         outputfile.write("<!doctype html>")
 
         # HTMLをパース
-        parser = indexHTMLParser(outputfile)
+        parser = indexHTMLParser()
         parser.feed(html_str)
+        outputstr = parser.get_outputstr()
+        outputfile.write(outputstr)
         parser.close()
 
     # print(parser.files)
 
-    for file_info in parser.files:
+    for file_info in parser.get_files():
         src_path = os.path.normpath(
             (os.path.join(input_dir, file_info["href"])).replace("\\", "/")
         )
