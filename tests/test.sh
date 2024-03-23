34c49a90f79c133c53542c019f8ab7c3ebcb1b6c
Yutaka Nishizaki <steelpipe75@gmail.com>
2024-03-23 13:26:50 +0000

    テストでやることをbashでまとめる

--- tests/test.sh
+++ tests/test.sh
@@ -1,9 +1,17 @@
 #!/bin/bashdata
 
+echo --- pip install ---
 python -m pip install -e ..
 
+echo --- c_cmake nomal ---
 python -m darumaotoshi -i ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/nomal
+echo --- c_cmake prettyprint ---
 python -m darumaotoshi -i ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/prettyprint -p
 
+echo --- cxx_cmake nomal ---
 python -m darumaotoshi -i ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/nomal
+echo --- cxx_cmake prettyprint ---
 python -m darumaotoshi -i ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/prettyprint -p
+
+echo --- diff ---
+diff -r GoldenFile/ work/
