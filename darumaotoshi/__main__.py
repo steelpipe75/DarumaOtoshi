6ffe0e2dcaca48befb42f6217aa41fdb190bfc17
Nishizaki <steelpipe75@gmail.com>
2024-03-23 20:52:08 +0900

    コマンドラインオプションでpretty_printで出力するかどうかを指定できるようにする

--- darumaotoshi/__main__.py
+++ darumaotoshi/__main__.py
@@ -4,8 +4,8 @@ from darumaotoshi import darumaotoshi
 
 
 def main():
-    input_index_html, output_dir = arg_parse()
-    darumaotoshi(input_index_html, output_dir)
+    input_index_html, output_dir, pretty_print = arg_parse()
+    darumaotoshi(input_index_html, output_dir, pretty_print)
 
 
 def arg_parse():
@@ -29,14 +29,24 @@ def arg_parse():
         help="output directory"
     )
 
+    # 出力ディレクトリを指定するオプションを追加
+    parser.add_argument(
+        "-p",
+        "--prettyprint",
+        dest="pretty_print",
+        default="false",
+        help="Pretty Print"
+    )
+
     # コマンドライン引数のパース
     args = parser.parse_args()
 
     print(f"Processing HTML file: {args.input_file}")
     print(f"Output directory: {args.output_dir}")
+    print(f"Pretty Print: {args.pretty_print}")
 
     # パースされた引数を使って処理を行う
-    return args.input_file, args.output_dir
+    return args.input_file, args.output_dir , args.pretty_print
 
 
 if __name__ == "__main__":
