08e577095d555fb2436998934555bee9eb0c5cee
Yutaka Nishizaki <steelpipe75@gmail.com>
2024-03-24 10:04:25 +0900

    flat test dev (#3)
    * flatのGoldenFileを追加
    
    * flatのテストを追加

--- tests/test.sh
+++ tests/test.sh
@@ -10,11 +10,15 @@ echo --- c_cmake nomal ---
 python -m darumaotoshi -i ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/nomal
 echo --- c_cmake prettyprint ---
 python -m darumaotoshi -i ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/prettyprint -p
+echo --- c_cmake flat ---
+python -m darumaotoshi -i ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/flat -f
 
 echo --- cxx_cmake nomal ---
 python -m darumaotoshi -i ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/nomal
 echo --- cxx_cmake prettyprint ---
 python -m darumaotoshi -i ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/prettyprint -p
+echo --- cxx_cmake flat ---
+python -m darumaotoshi -i ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/flat -f
 
 echo --- diff ---
 diff -r GoldenFile/ work/
