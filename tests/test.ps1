f6d6cf7197889fa2796c840af3ae112fce2103f4
Nishizaki <steelpipe75@gmail.com>
2024-03-30 17:47:15 +0900

    コマンドライン引数仕様に合わせてテスト処理を直し

--- tests/test.ps1
+++ tests/test.ps1
@@ -2,29 +2,29 @@ $scriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 Push-Location $scriptDirectory
 
 Write-Output "--- c_cmake nomal ---"
-python -m darumaotoshi -i .\data\c_cmake\bowling_game_cli\index.html -o .\work\c_cmake\nomal
+python -m darumaotoshi .\data\c_cmake\bowling_game_cli\index.html -o .\work\c_cmake\nomal
 Write-Output "--- c_cmake prettyprint ---"
-python -m darumaotoshi -i .\data\c_cmake\bowling_game_cli\index.html -o .\work\c_cmake\prettyprint -p
+python -m darumaotoshi .\data\c_cmake\bowling_game_cli\index.html -o .\work\c_cmake\prettyprint -p
 Write-Output "--- c_cmake flat ---"
-python -m darumaotoshi -i .\data\c_cmake\bowling_game_cli\index.html -o .\work\c_cmake\flat -f
+python -m darumaotoshi .\data\c_cmake\bowling_game_cli\index.html -o .\work\c_cmake\flat -f
 Write-Output "--- c_cmake embedded_css ---"
-python -m darumaotoshi -i .\data\c_cmake\bowling_game_cli\index.html -o .\work\c_cmake\embedded_css -e
+python -m darumaotoshi .\data\c_cmake\bowling_game_cli\index.html -o .\work\c_cmake\embedded_css -e
 Write-Output "--- c_cmake verbose ---"
-python -m darumaotoshi -i .\data\c_cmake\bowling_game_cli\index.html -o .\work\c_cmake\verbose -v
+python -m darumaotoshi .\data\c_cmake\bowling_game_cli\index.html -o .\work\c_cmake\verbose -v
 Write-Output "--- c_cmake option-all ---"
-python -m darumaotoshi -i .\data\c_cmake\bowling_game_cli\index.html -o .\work\c_cmake\option-all -p -f -e -v
+python -m darumaotoshi .\data\c_cmake\bowling_game_cli\index.html -o .\work\c_cmake\option-all -p -f -e -v
 
 Write-Output "--- cxx_cmake nomal ---"
-python -m darumaotoshi -i .\data\cxx_cmake\bowling_game_cli\index.html -o .\work\cxx_cmake\nomal
+python -m darumaotoshi .\data\cxx_cmake\bowling_game_cli\index.html -o .\work\cxx_cmake\nomal
 Write-Output "--- cxx_cmake prettyprint ---"
-python -m darumaotoshi -i .\data\cxx_cmake\bowling_game_cli\index.html -o .\work\cxx_cmake\prettyprint -p
+python -m darumaotoshi .\data\cxx_cmake\bowling_game_cli\index.html -o .\work\cxx_cmake\prettyprint -p
 Write-Output "--- cxx_cmake flat ---"
-python -m darumaotoshi -i .\data\cxx_cmake\bowling_game_cli\index.html -o .\work\cxx_cmake\flat -f
+python -m darumaotoshi .\data\cxx_cmake\bowling_game_cli\index.html -o .\work\cxx_cmake\flat -f
 Write-Output "--- cxx_cmake embedded_css ---"
-python -m darumaotoshi -i .\data\cxx_cmake\bowling_game_cli\index.html -o .\work\cxx_cmake\embedded_css -e
+python -m darumaotoshi .\data\cxx_cmake\bowling_game_cli\index.html -o .\work\cxx_cmake\embedded_css -e
 Write-Output "--- cxx_cmake verbose ---"
-python -m darumaotoshi -i .\data\cxx_cmake\bowling_game_cli\index.html -o .\work\cxx_cmake\verbose -v
+python -m darumaotoshi .\data\cxx_cmake\bowling_game_cli\index.html -o .\work\cxx_cmake\verbose -v
 Write-Output "--- cxx_cmake option-all ---"
-python -m darumaotoshi -i .\data\cxx_cmake\bowling_game_cli\index.html -o .\work\cxx_cmake\option-all -p -f -e -v
+python -m darumaotoshi .\data\cxx_cmake\bowling_game_cli\index.html -o .\work\cxx_cmake\option-all -p -f -e -v
 
 Pop-Location
--- tests/test.sh
+++ tests/test.sh
@@ -4,30 +4,30 @@ script_dir="$(dirname "$0")"
 pushd "$script_dir"
 
 echo --- c_cmake nomal ---
-python -m coverage run    -m darumaotoshi -i ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/nomal
+python -m coverage run    -m darumaotoshi ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/nomal
 echo --- c_cmake prettyprint ---
-python -m coverage run -a -m darumaotoshi -i ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/prettyprint -p
+python -m coverage run -a -m darumaotoshi ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/prettyprint -p
 echo --- c_cmake flat ---
-python -m coverage run -a -m darumaotoshi -i ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/flat -f
+python -m coverage run -a -m darumaotoshi ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/flat -f
 echo --- c_cmake embedded_css ---
-python -m coverage run -a -m darumaotoshi -i ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/embedded_css -e
+python -m coverage run -a -m darumaotoshi ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/embedded_css -e
 echo --- c_cmake verbose ---
-python -m coverage run -a -m darumaotoshi -i ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/verbose -v
+python -m coverage run -a -m darumaotoshi ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/verbose -v
 echo --- c_cmake option-all ---
-python -m coverage run -a -m darumaotoshi -i ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/option-all -p -f -e -v
+python -m coverage run -a -m darumaotoshi ./data/c_cmake/bowling_game_cli/index.html -o ./work/c_cmake/option-all -p -f -e -v
 
 echo --- cxx_cmake nomal ---
-python -m coverage run -a -m darumaotoshi -i ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/nomal
+python -m coverage run -a -m darumaotoshi ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/nomal
 echo --- cxx_cmake prettyprint ---
-python -m coverage run -a -m darumaotoshi -i ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/prettyprint -p
+python -m coverage run -a -m darumaotoshi ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/prettyprint -p
 echo --- cxx_cmake flat ---
-python -m coverage run -a -m darumaotoshi -i ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/flat -f
+python -m coverage run -a -m darumaotoshi ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/flat -f
 echo --- cxx_cmake embedded_css ---
-python -m coverage run -a -m darumaotoshi -i ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/embedded_css -e
+python -m coverage run -a -m darumaotoshi ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/embedded_css -e
 echo --- cxx_cmake verbose ---
-python -m coverage run -a -m darumaotoshi -i ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/verbose -v
+python -m coverage run -a -m darumaotoshi ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/verbose -v
 echo --- cxx_cmake option-all ---
-python -m coverage run -a -m darumaotoshi -i ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/option-all -p -f -e -v
+python -m coverage run -a -m darumaotoshi ./data/cxx_cmake/bowling_game_cli/index.html -o ./work/cxx_cmake/option-all -p -f -e -v
 
 echo --- coverage htm ---
 python -m coverage html
